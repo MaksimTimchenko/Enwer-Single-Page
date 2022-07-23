551e04b43e2b6a0e739ec70d3f574f523baf54ee
unknown <maximtimchenko746@gmail.com>
2022-07-24 00:43:32 +0300

    Add project

---
+++ script.js
@@ -0,0 +1,51 @@
+const burgerBtn = document.querySelector('.header-burger'),
+      navMenu = document.querySelector('.nav-menu');
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})     
+
+
+//cards
+
+class MenuCard {
+    constructor (img, title, description, parentSelector) {
+        this.img = img;
+        this.title = title;
+        this.description = description;
+        this.parent = document.querySelector(parentSelector)
+
+    }
+
+    render () { 
+        const element = document.createElement('div')
+        element.innerHTML = `
+        <div class="card">
+            
+            <img src="${this.img}" alt="">
+            <span>${this.title}</span>
+            <p>${this.description}</p>
+        </div>
+        `;
+        this.parent.append(element)
+    }   
+}
+
+fetch('http://localhost:3000/cards')
+    .then((response) => {
+        return response.json()
+    })
+    .then(data => {
+        console.log('ddd');
+        data.forEach(({
+            img,
+            title,
+            description
+        }) => {
+           new MenuCard(img, title ,description,'.services-cards').render();
+        });
+    });
+
+
+
